c86f14162e0b4d0125e0beb43328fd96ef98a3e9
TJ <michael.btj@hotmail.com>
2022-11-26 21:18:09 -0500

    import components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { observer } from 'mobx-react-lite';
-
+import Guess from "../components/Guess";
+import Qwerty from "../components/Qwerty";
+import PuzzleStore from '../stores/PuzzleStore';
 export default observer(function Home() {
   const store = useLocalObservable (() => PuzzleStore)
   return <div>
     <h1>Wordle</h1>
     <Guess/>
     <h1>won/loss</h1>
-    <Querty/>
+    <Qwerty/>
   </div>
 })
