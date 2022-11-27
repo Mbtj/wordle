f3ed17eeb949b6eec5559605b68d64f98dc358ff
TJ <michael.btj@hotmail.com>
2022-11-27 06:29:20 -0500

    styling: add flex

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,12 @@ import Qwerty from "../components/Qwerty";
 import PuzzleStore from '../stores/PuzzleStore';
 export default observer(function Home() {
   const store = useLocalObservable (() => PuzzleStore)
-  return <div>
-    <h1>Wordle</h1>
-    <Guess/>
-    <h1>won/loss</h1>
-    <Qwerty/>
-  </div>
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
+      <h1>Wordle</h1>
+      <Guess/>
+      <h1>won/loss</h1>
+      <Qwerty/>
+    </div>
+  )
 })
