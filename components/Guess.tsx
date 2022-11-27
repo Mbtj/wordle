0a6d0a0c7bdc5cb0e321bc3f6103dbe98627c9d5
TJ <michael.btj@hotmail.com>
2022-11-27 06:41:13 -0500

    style guess component

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -1,3 +1,9 @@
-export default function Guess() {
-  return <div>guess</div>
+export default function Guess({isGuessed, guess, word}) {
+  return (
+  <div className="grid grid-cols-5 gap-1 mb-2">
+    {new Array(5).fill(0).map((_, i) => (
+      <div className="h-16 w-16 border-gray-400 bg-black text-white flex uppercase items-center font-bold justify-center">{guess[i]}</div>
+    ))}
+  </div>
+  )
 }
