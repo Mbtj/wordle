9bb5cc33c3fbb00f95b3369bad030c21581882ae
TJ <michael.btj@hotmail.com>
2022-11-27 06:52:53 -0500

    finish guess states

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -1,9 +1,18 @@
 export default function Guess({isGuessed, guess, word}) {
+  
   return (
   <div className="grid grid-cols-5 gap-1 mb-2">
-    {new Array(5).fill(0).map((_, i) => (
-      <div className="h-16 w-16 border-gray-400 bg-black text-white flex uppercase items-center font-bold justify-center">{guess[i]}</div>
-    ))}
+    {new Array(5).fill(0).map((_, i) => {
+      const bgColor = !isGuessed
+      ? 'bg-black'
+      : guess[i] === word[i]
+      ? 'bg-green-400'
+      : word.includes(guess[i])
+      ? 'bg-yellow-400'
+      : 'bg-black'
+  
+     return <div className={`h-16 w-16 border-gray-400 bg-black text-white flex uppercase items-center font-bold justify-center ${bgColor}`}>{guess[i]}</div>
+})}
   </div>
   )
 }
