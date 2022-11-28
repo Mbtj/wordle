2bc5d239e1d1b4a3d4cc23bc9a63c21608a9f76d
TJ <michael.btj@hotmail.com>
2022-11-28 10:38:36 -0500

    add keyhandling functions

--- stores/PuzzleStore.tsx
+++ stores/PuzzleStore.tsx
@@ -15,6 +15,29 @@ export default {
     this.word = words[Math.round(Math.random() * words.length)];
     this.guesses.replace(new Array(6).fill(''));
     this.currentGuess = 0;
+  },
+
+  submitGuess() {
+    if (words.includes(this.guesses[this.currentGuess])) {
+      this.currentGuess += 1;
+    }
+  },
+
+  handleKeyup(e) {
+    if (this.won || this.lost) {
+      return
+    }
+
+    if (e.key === "Enter") {
+    return this.submitGuess()
+  }
+
+  if (e.key === "Backspace") {
+    this.guesses[this.currentGuess] = this.guesses[this.currentGuess].slice(0, this.guesses[this.currentGuess].length - 1 )
+    return
+  }
+  if (this.guesses[this.currentGuess].length < 5 && e.key.match(/[A-z]/)) {
+    this.guesses[this.currentGuess] = this.guesses[this.currentGuess] + e.key.toLowerCase()
   }
 }
-
+}
