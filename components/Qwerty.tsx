8e6113c5889c5603b797e353e8022ca0acd1d6c0
TJ <michael.btj@hotmail.com>
2022-11-28 10:59:39 -0500

    create qwerty keyboard interface

--- components/Qwerty.tsx
+++ components/Qwerty.tsx
@@ -1,3 +1,16 @@
 export default function Qwerty() {
-  return <div>qwerty</div>
+  const qwerty = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+  return (
+    <div>
+      {qwerty.map((row) => (
+        <div className="flex justify-center">
+          {row.split('').map((key) => (
+            <div className="m-px w-10 h-10 bg-gray-400 rounded-md flex items-center justify-center uppercase">
+              {key}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
 }
