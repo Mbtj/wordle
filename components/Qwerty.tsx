6f6d6e752085564a6db9f55f651b0aa8aa52e90a
TJ <michael.btj@hotmail.com>
2022-11-28 11:44:25 -0500

    update qwerty component

--- components/Qwerty.tsx
+++ components/Qwerty.tsx
@@ -1,16 +1,28 @@
-export default function Qwerty() {
+import { observer } from "mobx-react-lite";
+
+export default observer(function Qwerty({ store }) {
   const qwerty = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
   return (
     <div>
       {qwerty.map((row) => (
         <div className="flex justify-center">
-          {row.split('').map((key) => (
-            <div className="m-px w-10 h-10 bg-gray-400 rounded-md flex items-center justify-center uppercase">
-              {key}
+          {row.split('').map((char) => {
+            const bgColor = store.exactGuesses.includes(char)
+             ? "bg-green-400"
+             : store.inexactGuesses.includes(char)
+             ? "bg-yellow-400"
+             : store.allGuesses.includes(char)
+             ? 'bg-gray-400'
+             : 'bg-gray-200'
+             ;
+            return (
+              <div className={`m-px w-10 h-10 rounded-md flex items-center justify-center uppercase ${bgColor}`}>
+              {char}
             </div>
-          ))}
+              )
+          })}
         </div>
       ))}
     </div>
   )
-}
+})
