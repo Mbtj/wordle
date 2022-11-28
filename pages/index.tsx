3f40cc0e4c7c0773f6ebef89c42e8cf392dbdbb8
TJ <michael.btj@hotmail.com>
2022-11-28 10:38:49 -0500

    add keyup handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,14 @@ export default observer(function Home() {
   const store = useLocalObservable (() => PuzzleStore)
   useEffect(() => {
     store.init()
+    window.addEventListener('keyup', store.handleKeyup);
+
+    //clean up function
+    return () => {
+      window.removeEventListener('keyup', store.handleKeyup);
+    }
   }, [])
 
-  console.log(store.guesses)
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
       <h1 className="text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">Wordle</h1>
