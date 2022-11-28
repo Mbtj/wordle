84aa8e393c7bf744e7dfb20eeed3254472cf00c1
TJ <michael.btj@hotmail.com>
2022-11-28 10:27:03 -0500

    create stores

--- stores/PuzzleStore.tsx
+++ stores/PuzzleStore.tsx
@@ -1,1 +1,20 @@
-export default {}
+import words from "../words.json";
+
+export default {
+  word: '',
+  guesses: [],
+  currentGuess: 0,
+  get won() {
+    return this.guesses[this.currentGuess - 1] === this.word;
+  },
+
+  get lost() {
+    return this.currentGuess === 6;
+  },
+  init() {
+    this.word = words[Math.round(Math.random() * words.length)];
+    this.guesses.replace(new Array(6).fill(''));
+    this.currentGuess = 0;
+  }
+}
+
