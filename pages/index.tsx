0d8535a8e324392552a39e4dcd89b4a37e83d701
TJ <michael.btj@hotmail.com>
2022-11-28 10:27:39 -0500

    update guess props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
 import { observer, useLocalObservable } from 'mobx-react-lite';
+import { useEffect } from 'react';
 import Guess from "../components/Guess";
 import Qwerty from "../components/Qwerty";
 import PuzzleStore from '../stores/PuzzleStore';
 export default observer(function Home() {
   const store = useLocalObservable (() => PuzzleStore)
+  useEffect(() => {
+    store.init()
+  }, [])
+
+  console.log(store.guesses)
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
       <h1 className="text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">Wordle</h1>
-      <Guess word={"texts"} guess={"guess"} isGuessed={false}/>
+      {new Array(6).fill(0).map((_, i) => (
+        <Guess
+          key={i}
+          word={store.word}
+          guess={store.guesses[i]}
+          isGuessed={i < store.currentGuess}
+        />
+      ))}
       <h1>won/loss</h1>
       <Qwerty/>
+      {store.word}
     </div>
   )
 })
